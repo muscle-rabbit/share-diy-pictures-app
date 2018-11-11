9aeea2ee5d5e7e317cc0b8fe8300e83ec5e228e7
shinyamizuno1008 <bitsukun75@gmail.com>
2018-11-12 07:27:55 +0900

    add: ImageList にonPressメソッドを実装しました。

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,29 +1,41 @@
 import * as React from 'react';
-import { View, ImageURISource, Image, Dimensions } from 'react-native';
+import {
+  View,
+  ImageURISource,
+  Image,
+  Dimensions,
+  TouchableHighlight
+} from 'react-native';
 
 type Props = {
   images: ImageURISource[];
+  onPress: (i: number) => void;
 };
 
 const ImageList: React.SFC<Props> = (props: Props) => {
-  console.log(Dimensions.get('window').width / 3);
   const screenWidth = Dimensions.get('window').width;
   return (
     <View
       style={{ flexDirection: 'row', width: screenWidth, flexWrap: 'wrap' }}
     >
       {props.images.map((image, index) => (
-        <Image
-          key={index}
-          style={{
-            height: 130,
-            flexGrow: 1,
-            width: 140,
-            margin: 1
+        <TouchableHighlight
+          // tslint:disable-next-line
+          onPress={() => {
+            props.onPress(index);
           }}
-          resizeMode={'cover'}
-          source={image}
-        />
+          style={{ flexGrow: 1, width: 140, margin: 1 }}
+        >
+          <Image
+            key={index}
+            style={{
+              height: 130,
+              width: 'auto'
+            }}
+            resizeMode={'cover'}
+            source={image}
+          />
+        </TouchableHighlight>
       ))}
     </View>
   );
